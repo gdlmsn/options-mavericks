7ea593042a288867468a427158b3dfdc61b03c46
gdlmsn <gdalmasson@gmail.com>
2023-07-08 21:17:16 +1000

    next-auth config changes

--- lib/session.ts
+++ lib/session.ts
@@ -1,9 +1,76 @@
-import { getServerSession } from "next-auth/next"
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
+import bcrypt from "bcrypt"
+import NextAuth, { NextAuthOptions, getServerSession } from "next-auth"
+import CredentialsProvider from "next-auth/providers/credentials"
+import DiscordProvider from "next-auth/providers/discord"
+import GithubProvider from "next-auth/providers/github"
+import GoogleProvider from "next-auth/providers/google"
 
-import { authOptions } from "@/app/api/auth/[...nextauth]/route"
+import { SessionInterface } from "@/types/next-auth"
+import prisma from "@/lib/prismadb"
+
+// For more information on each option (and a full list of options) go to
+// https://next-auth.js.org/configuration/options
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  pages: {
+    signIn: "/login",
+  },
+  // https://next-auth.js.org/configuration/providers/oauth
+  providers: [
+    DiscordProvider({
+      clientId: process.env.DISCORD_CLIENT_ID as string,
+      clientSecret: process.env.DISCORD_CLIENT_SECRET as string,
+    }),
+    GithubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID as string,
+      clientSecret: process.env.GOOGLE_SECRET as string,
+    }),
+
+    CredentialsProvider({
+      name: "credentials",
+      credentials: {
+        email: { label: "Email", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials.password) {
+          throw new Error("Wrong credentials")
+        }
+        const user = await prisma.user.findUnique({
+          where: {
+            email: credentials.email,
+          },
+        })
+
+        if (!user || !user?.hashedPassword) {
+          throw new Error("Invalid credentials")
+        }
+
+        const isCorrectPassword = await bcrypt.compare(
+          credentials.password,
+          user.hashedPassword
+        )
+
+        if (!isCorrectPassword) {
+          throw new Error("Invalid credentials")
+        }
+        return user
+      },
+    }),
+  ],
+  debug: process.env.NODE_ENV === "development",
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+}
 
 export async function getCurrentUser() {
-  const session = await getServerSession(authOptions)
-
-  return session?.user
+  const session = (await getServerSession(authOptions)) as SessionInterface
+  return session
 }
